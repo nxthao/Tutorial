4d75a6a62f829e23437f3c88c13ddce950293181
tnqt <nxthao0701@gmail.com>
2019-09-15 06:21:01 +0700

    Update game: add the times played

--- LearnC++/Basics/Practice14_PlayGameGuessNumber.cpp
+++ LearnC++/Basics/Practice14_PlayGameGuessNumber.cpp
@@ -4,12 +4,13 @@
 #include <ctime> // for std::time
 
 int a, b;
+char c;
 bool result;
 
 int GetRandomNumber()
 {
 	std::mt19937 mersenne { static_cast<std::mt19937::result_type>(std::time(nullptr))};
-	std::uniform_int_distribution<> rand(1,10);
+	std::uniform_int_distribution<> rand(1,80);
 	return rand(mersenne);
 }
 
@@ -23,11 +24,30 @@ int GuessNumber()
 
 bool CheckNumber(int tmp1, int tmp2)
 {
-	return a == b ? 1 : 0;
+	int i = 0;
+	while (i++ < 7)
+	{
+		if (tmp1 == tmp2)
+		{
+			return 1;
+		}
+		else if (tmp1 < tmp2)
+		{
+			std::cout << "You submit the value higher than the expected value. Submit again times: " << i << "\n";
+			tmp2 = GuessNumber();
+		}
+		else
+		{
+			std::cout << "You submit the value lower than the expected value. Submit again times: " << i << "\n";
+			tmp2 = GuessNumber();
+		}
+	}	
+	return 0;
 }
 
 void main()
 {
+next:
 	a = GetRandomNumber();
 	b = GuessNumber();
 	result = CheckNumber(a, b);
@@ -38,5 +58,20 @@ void main()
 	else
 	{
 		std::cout << "You guess wrongly a = " << a << "\n";
+	}
+	std::cout << "Do you play again? y/n \n";
+	std::cin >> c;
+	if (c == 'y')
+	{
+		std::cout << "welcome you again \n";
+		goto next;
+	}
+	else if (c == 'n')
+	{
+		std::cout << "See you again \n";
+	}
+	else
+	{
+		std::cout << "You submit wrongly, so see you again \n";
 	}
 }
