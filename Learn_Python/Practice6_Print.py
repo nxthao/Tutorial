e930cba9c4ff1bd5e402dd463db643b32446a6e6
tnqt <nxthao0701@gmail.com>
2019-09-29 22:39:09 +0700

    Basic print

---
+++ Learn_Python/Practice6_Print.py
@@ -0,0 +1,14 @@
+print("If I don't want to a new line: use \"end = ' ' \" ")
+print('hello', 'world', 4545, sep ='%%')
+for i in range(0,5):
+    print ("Hello")
+
+a = 7
+print (a)
+
+print ("address of variable use \"id\"")
+print(id(a))
+
+print ("If I add a variable, it will have diffenence address")
+a = 8
+print(id(a))
