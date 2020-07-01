a1df94b31df8efae151c6db39b804bab4846599f
TN <nxthao0701@gmail.com>
2020-07-01 09:43:09 +0700

    [IOS] Update method example.

--- LearnIOS/Chapter12/exampleMethod.playground/Contents.swift
+++ LearnIOS/Chapter12/exampleMethod.playground/Contents.swift
@@ -3,7 +3,7 @@ import UIKit
 print("How many months until the winter break: ")
 
 struct SimpleDay{
-    let currentMonth : String
+    var currentMonth = "January"
     func monthsUntilWinterBreak() -> Int?{
         months.firstIndex(of: "December")! - months.firstIndex(of: self.currentMonth)!
     }
@@ -14,5 +14,31 @@ let months = [
     "July", "August", "September", "October", "November", "December"
 ]
 
+let monthsDefault = SimpleDay()
+print("\(monthsDefault.monthsUntilWinterBreak()!) months.")
 let monthsFromJuly = SimpleDay(currentMonth: "July")
-print("\(monthsFromJuly.monthsUntilWinterBreak()!) months")
+print("\(monthsFromJuly.monthsUntilWinterBreak()!) months.")
+
+
+print("Type method.")
+// Unable to access to property when using Static in method
+print("Factorial does n't use type method.")
+struct Math{
+    var number : Int
+
+    func factorial() -> Int{
+        (1...self.number).reduce(1, *)
+    }
+}
+let result = Math(number: 6)
+print(result.factorial())
+
+print("Factorial uses type method (Static).")
+struct MathTypeMethod{
+    static func factorial(number : Int) -> Int{
+        (1...number).reduce(1, *)
+    }
+}
+print(MathTypeMethod.factorial(number: 6))
+
+
