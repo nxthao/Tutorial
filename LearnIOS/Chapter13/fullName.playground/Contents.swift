9ad58ee70aa85cb44a0beb27a7a921ee3ca561f4
TN <nxthao0701@gmail.com>
2020-07-06 23:24:14 +0700

    [IOS] Full name with class

---
+++ LearnIOS/Chapter13/fullName.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class Person{
+    var firstName : String
+    var lastName : String
+    
+    init(firstName : String, lastName : String) {
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+    
+    var fullName : String{
+        "\(firstName) \(lastName)"
+    }
+}
+
+let TN = Person(firstName: "Thao", lastName: "Nguyen")
+print("Fullname: \(TN.fullName)")
+    
