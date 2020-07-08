531a9f947c932ae53cdfb6ebf89c032cab240725
TN <nxthao0701@gmail.com>
2020-07-08 22:31:54 +0700

    [IOS] Objecg identity

--- LearnIOS/Chapter13/exampleClass.playground/Contents.swift
+++ LearnIOS/Chapter13/exampleClass.playground/Contents.swift
@@ -22,6 +22,8 @@ TN.firstName = "T"
 var origin = TN
 print("First name: \(TN.firstName)")
 print("First name of origin: \(origin.firstName)")
+print("Come back default.")
+TN.firstName = "Thao"
 
 
 print("Object identity.")
@@ -29,7 +31,7 @@ if TN === origin{
     print("OK")
 }
 
-var multiplePeople = Person(firstName: "Thao", lastName: "Xuan")
+var multiplePeople = Person(firstName: "Thao", lastName: "Nguyen")
 if TN === multiplePeople{
     print("TN is same multiplePeople.")
 }
@@ -38,5 +40,20 @@ else{
 }
 
 var imposters = (0...100).map{_ in
-    Person(firstName: "Thao", lastName: "Xuan")
+    Person(firstName: "Thao", lastName: "Nguyen")
 }
+
+print(imposters.contains{$0.firstName == TN.firstName && $0.lastName == TN.lastName})
+
+print(imposters.contains(where: {$0 === TN})) // false
+
+// Insert TN to imposter
+imposters.insert(TN, at: Int.random(in: 0...100))
+
+print(imposters.contains(where: {$0 === TN})) // true
+
+if let indexTN = imposters.firstIndex(where: {$0 === TN}){
+    imposters[indexTN].firstName = "T"
+    imposters[indexTN].lastName = "N"
+}
+print(TN.fullName)
