f3daa09a163b73e66c4100bf25af87756c5c8744
TN <nxthao0701@gmail.com>
2020-07-10 23:02:08 +0700

    [IOS] Update moving view app

--- LearnIOS/Chapter13/movingViewApp.playground/Contents.swift
+++ LearnIOS/Chapter13/movingViewApp.playground/Contents.swift
@@ -17,21 +17,38 @@ class List{
 }
 
 let tn = List(name: "Thao Nguyen", movieTitle: ["Descendants of the sun", "Iris"])
-tn.printList()
+//tn.printList()
 
 class User{
-    var list : [String : [String]] = [:]
+    var list : [String : List] = [:]
     func addList(user: List){
-        list[user.name] = user.movieTitle
+        list[user.name] = user
     }
     
     func listName(forName : String) -> List?{
-//        guard list.isEmpty else {
-//            return nil
-//        }
-//        if list.contains(where: $0.keys == forName){
-//            return
-//        }
-        List[forName]
+        if list.contains(where: {$0.key == forName}){
+            return list[forName]
+        }
+        else{
+            return nil
+        }
     }
 }
+
+let jane = User()
+let john = jane
+jane.addList(user: tn)
+john.addList(user: tn)
+
+jane.addList(user: List(name: "Dao Hieu", movieTitle: ["Rambo"]))
+
+john.listName(forName: "Thao Nguyen")?.movieTitle.append("Terminator")
+
+print("With name \"Thao Nguyen\", John should have: Descendants of the sun, Iris, Terminator")
+john.listName(forName: "Thao Nguyen")?.printList()
+
+print("With name \"Thao Nguyen\", Jane should have: Descendants of the sun, Iris, Terminator")
+jane.listName(forName: "Thao Nguyen")?.printList()
+
+print("With name \"Dao Hieu\", Jane should have: Rambo")
+jane.listName(forName: "Dao Hieu")?.printList()
