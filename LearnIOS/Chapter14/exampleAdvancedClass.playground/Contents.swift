1d5746c4e3e6dbc9910cf98dbe4dafafe926099e
TN <nxthao0701@gmail.com>
2020-07-17 21:41:22 +0700

    [IOS] Update advanced class example

--- LearnIOS/Chapter14/exampleAdvancedClass.playground/Contents.swift
+++ LearnIOS/Chapter14/exampleAdvancedClass.playground/Contents.swift
@@ -15,10 +15,14 @@ class Person{
 }
 
 
+struct Grade{
+    let letter : String
+    let point : Int
+}
 class Student : Person{
-    let grade : Int
+    let grade : Grade
     
-    init(firstName : String, lastName : String, grade : Int) {
+    init(firstName : String, lastName : String, grade : Grade) {
         self.grade = grade
         super.init(firstName: firstName, lastName: lastName)
     }
@@ -27,12 +31,17 @@ class Student : Person{
     func printInformation(){
         print("\(super.fullName()) are in grade \(self.grade)")
     }
+    
+    var grades : [Grade] = []
+    func recordGrade(){
+        grades.append(self.grade)
+    }
 }
 
 
 let nn = Person(firstName: "Nguyen", lastName: "Nguyen")
 print(nn.fullName())
-let tn = Student(firstName: "Thao", lastName: "Nguyen", grade: 20)
+let tn = Student(firstName: "Thao", lastName: "Nguyen", grade: Grade(letter: "A", point: 10))
 tn.printInformation()
 
 
@@ -56,10 +65,43 @@ func phoneBookName(_ person : Person){
     print("\(person.fullName())")
 }
 var t = Person(firstName: "Thao", lastName: "Nguyen")
-var n = obeyMember(firstName: "Nguyen", lastName: "Nguyen", grade: 20)
+var n = obeyMember(firstName: "Nguyen", lastName: "Nguyen", grade: Grade(letter: "A", point: 10))
 
 // With a function but give many results.
 phoneBookName(t)
 phoneBookName(n)
 
 
+print("Hierarchy check.")
+
+var hierarchyCheck = Student(firstName: "hierarchy", lastName: "check", grade: Grade(letter: "C", point: 6))
+
+hierarchyCheck as? BandMember
+print((hierarchyCheck as? BandMember)?.minimumPracticeTime)
+
+func afterClassActivity(person : Student){
+    print("Go home.")
+}
+
+func afterClassActivity(person : BandMember){
+    print("Go to practice.")
+}
+
+afterClassActivity(person: n as Student)
+afterClassActivity(person: n as BandMember)
+
+
+print("Overide")
+
+class StudentAthelete : Student{
+    var studenFailed : [Grade] = []
+    override func recordGrade() {
+        super.recordGrade()
+    }
+    
+}
+
+var member = StudentAthelete(firstName: "Dao", lastName: "Nguyen", grade: Grade(letter: "A", point: 9))
+print(member.fullName())
+member.recordGrade()
+print(member.grades)
