5de0b4b8f06e36381143f82ffb1345b108e03303
TN <nxthao0701@gmail.com>
2020-07-28 07:54:52 +0700

    [IOS] Update enum example

--- LearnIOS/Chapter15/enumExample.playground/Contents.swift
+++ LearnIOS/Chapter15/enumExample.playground/Contents.swift
@@ -29,12 +29,51 @@ func season(month : Month) -> String{
     }
 }
 
+let jan = Month.january
+print(jan)
+print("Which season is january? \(season(month: jan))")
+
+print("Raw value")
 // To use raw value, need to clare it in decalare of enum.( If declare Int: raw will be Int, if String, raw will be String)
 func monthsUntilNewYear(month : Month) -> Int{
     Month.december.rawValue - month.rawValue
 }
-
-let jan = Month.january
-print(jan)
-print("Which season is january? \(season(month: jan))")
 print(monthsUntilNewYear(month: jan))
+
+
+print("Unorder raw value")
+enum Coin : Int{
+    case penny = 1
+    case nickel = 5
+    case quarter = 25
+}
+
+let coin = Coin.quarter.rawValue
+print("Coin: \(coin)")
+
+// Associate a custom value (or values) with each enumeration case
+print("Associated values")
+
+var balance = 50
+enum WithDrawResult{
+    case success(newBalance : Int)
+    case error(message: String)
+}
+
+func withDraw(amount : Int) -> WithDrawResult{
+    if balance >= amount{
+        balance -= amount
+        return .success(newBalance: balance)
+    }
+    else{
+        return . error(message: "The money is not enough in ATM card")
+    }
+}
+
+let money = withDraw(amount: 70)
+switch money{
+case .success(newBalance: let newBalance):
+    print("The money remain: \(newBalance)")
+case .error(message: let message):
+    print(message)
+}
