d4cad757d3ecdf028b1ab8da461d2e3e6c69f4ef
TN <nxthao0701@gmail.com>
2020-08-03 23:22:56 +0700

    [IOS] Update protocol example

--- LearnIOS/Chapter16/protocolExample.playground/Contents.swift
+++ LearnIOS/Chapter16/protocolExample.playground/Contents.swift
@@ -42,3 +42,39 @@ class AnotherBike : WheeledVehicle{
         brakesApplied = true
     }
 }
+
+
+print("Protocol in library standard")
+
+class Record : Equatable{
+    let wins : Int
+    let loses : Int
+    
+    init(wins : Int, loses : Int) {
+        self.wins = wins
+        self.loses = loses
+    }
+
+    static func ==(lhs : Record, rhs : Record) -> Bool{
+        lhs.wins == rhs.wins && lhs.loses == rhs.loses
+    }
+}
+
+let recordA = Record(wins: 7, loses: 2)
+let recordB = Record(wins: 7, loses: 2)
+print(recordA == recordB)
+
+extension Record{
+    static func <(lhs : Record, rhs : Record) -> Bool{
+        if lhs.wins == rhs.wins{
+            return lhs.loses > rhs.loses
+        }
+        return lhs.wins < rhs.wins
+    }
+}
+
+print(recordA < recordB)
+let recordC = Record(wins: 10, loses: 5)
+
+
+
