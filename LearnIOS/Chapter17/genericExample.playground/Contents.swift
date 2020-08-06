1ec414311c1f99c6818178b125a346607f36cd78
TN <nxthao0701@gmail.com>
2020-08-07 06:36:42 +0700

    [IOS] Update Generic example

--- LearnIOS/Chapter17/genericExample.playground/Contents.swift
+++ LearnIOS/Chapter17/genericExample.playground/Contents.swift
@@ -20,3 +20,32 @@ swapValue(a: &stringValue1, b: &stringValue2)
 print("After swapping: stringValue1: \(stringValue1), stringValue2: \(stringValue2)")
 
 
+print("Generic type")
+
+struct Stack<Type>{
+    var items : [Type]
+    mutating func push(item : Type) -> [Type]{
+        items.append(item)
+        return items
+    }
+    
+    mutating func pop() -> [Type]{
+        items.removeLast()
+        return items
+    }
+}
+
+var emptyString : [String] = []
+var stackString = Stack<String>(items: emptyString)
+stackString.push(item: "Thao")
+stackString.push(item: "Nguyen")
+stackString.push(item: "Dao")
+stackString.push(item: "Hieu")
+print("Current Stack: \(stackString.items)")
+stackString.push(item: "A")
+print("Current Stack: \(stackString.items)")
+stackString.pop()
+print("Stack after poping: \(stackString.items)")
+
+
+
