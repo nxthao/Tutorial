bba433d3e73eb4b137472fa5508f72e57054cab0
TN <nxthao0701@gmail.com>
2020-08-27 06:20:49 +0700

    [IOS] Basic application

---
+++ Application/BasicIOSApplication/BasicIOSApplication/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BasicIOSApplication
+//
+//  Created by Thao Nguyen on 8/26/20.
+//  Copyright © 2020 Thao Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello Thao Nguyen").fontWeight(.bold).foregroundColor(.blue)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
