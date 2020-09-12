59e98498ee9d845b2046763266a4cc219f9dcbb8
TN <nxthao0701@gmail.com>
2020-09-12 12:51:05 +0700

    [IOS] Update to-do-list app

---
+++ Application/ToDoList/ToDoList/EditCheckListItemView.swift
@@ -0,0 +1,21 @@
+//
+//  EditCheckListItemView.swift
+//  ToDoList
+//
+//  Created by Thao Nguyen on 9/12/20.
+//  Copyright © 2020 Thao Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct EditCheckListItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditCheckListItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditCheckListItemView()
+    }
+}
