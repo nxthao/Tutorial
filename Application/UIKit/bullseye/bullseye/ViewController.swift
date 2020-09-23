f3518bb7a537f2dbb6f22392ed9510bb52c6d4bb
TN <nxthao0701@gmail.com>
2020-09-23 23:09:55 +0700

    [IOS] UPdate app

--- Application/UIKit/bullseye/bullseye/ViewController.swift
+++ Application/UIKit/bullseye/bullseye/ViewController.swift
@@ -13,7 +13,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func showAlert(){
+        let alert = UIAlertController(title: "Hello everyone", message: "This button is pushed", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
