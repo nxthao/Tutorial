ee239394b7c477a386b8364b9531032ff10e1db8
TN <nxthao0701@gmail.com>
2020-09-28 23:00:57 +0700

    [IOS] Update app

--- Application/UIKit/bullseyeUIKit/bullseyeUIKit/ViewController.swift
+++ Application/UIKit/bullseyeUIKit/bullseyeUIKit/ViewController.swift
@@ -14,32 +14,59 @@ class ViewController: UIViewController {
     
     var currentValue = 50
     var targetValue = 0
+    var score = 100
+    var round = 1
     
     @IBOutlet weak var slider : UISlider!
+    @IBOutlet weak var targetLabel : UILabel!
+    @IBOutlet weak var scoreLabel : UILabel!
+    @IBOutlet weak var roundLabel : UILabel!
     
     // Methods
     // =======
     
     func startNewRound(){
-        targetValue = Int.random(in: 0...100)
-        currentValue = Int.random(in: 0...100)
+        randomCurrentTargetValue()
         slider.value = Float(currentValue)
+        updateLabels()
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         startNewRound()
-//        currentValue = lroundf(slider.value)
-//        targetValue = Int.random(in: 0...100)
-        // Do any additional setup after loading the view.
     }
+    
+    func randomCurrentTargetValue(){
+        targetValue = Int.random(in: 0...100)
+        currentValue = Int.random(in: 0...100)
+    }
+    
+    func updateLabels(){
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
+    func totalScore(){
+        let difference = abs(targetValue - currentValue)
+        score = score - difference
+    }
+    
+    func alertAction(){
+        totalScore()
+        round += 1
+        startNewRound()
+    }
+    
     @IBAction func showAlert(){
-        let message = "The value of slider is \(currentValue)"
+        let message = "The point: \(score)\n" +
+                      "The value of slider: \(currentValue)"
         let alert = UIAlertController(title: "Hello Thao Nguyen", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome!", style: .default, handler: nil)
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        alertAction()
     }
     
     @IBAction func sliderMoved(_ slider : UISlider){
