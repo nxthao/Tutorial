9207a5fdb4ac63442388faba87aed0338a68bee3
TN <nxthao0701@gmail.com>
2020-11-21 23:33:54 +0700

    [IOS] Edit and group location

--- Application/UIKit/MyMap/MyMap/LocationsViewController.swift
+++ Application/UIKit/MyMap/MyMap/LocationsViewController.swift
@@ -12,19 +12,24 @@ import CoreLocation
 class LocationsViewController: UITableViewController, NSFetchedResultsControllerDelegate {
     var managedObjectContext : NSManagedObjectContext!
     // Location variable
-//    var locations = [Location]()
+    //    var locations = [Location]()
     lazy var fetchedResultsController : NSFetchedResultsController<Location> = {
         let fetchRequest = NSFetchRequest<Location>()
         
         let entity = Location.entity()
         fetchRequest.entity = entity
         
-        let sortDescriptor = NSSortDescriptor(key: "date", ascending: true)
-        fetchRequest.sortDescriptors = [sortDescriptor]
+        let sort1 = NSSortDescriptor(key: "caregory", ascending: true)
+        let sort2 = NSSortDescriptor(key: "date", ascending: true)
+        fetchRequest.sortDescriptors = [sort1, sort2]
         
         fetchRequest.fetchBatchSize = 20
         
-        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext, sectionNameKeyPath: nil, cacheName: "Locations"
+        let fetchedResultsController = NSFetchedResultsController(
+            fetchRequest: fetchRequest,
+            managedObjectContext: self.managedObjectContext,
+            sectionNameKeyPath: "category",
+            cacheName: "Locations"
         )
         fetchedResultsController.delegate = self
         return fetchedResultsController
@@ -33,79 +38,78 @@ class LocationsViewController: UITableViewController, NSFetchedResultsController
     override func viewDidLoad() {
         super.viewDidLoad()
         performFetch()
-//        let fetchRequest = NSFetchRequest<Location>()
-//        let entity = Location.entity()
-//        fetchRequest.entity = entity
-//        let sortDescription = NSSortDescriptor(key: "date", ascending: true)
-//        fetchRequest.sortDescriptors = [sortDescription]
-//        do {
-//            locations = try managedObjectContext.fetch(fetchRequest)
-//        } catch {
-//            fatalCoreDataError(error)
-//        }
+        navigationItem.rightBarButtonItem = editButtonItem
     }
     
     deinit{
         fetchedResultsController.delegate = nil
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return fetchedResultsController.sections!.count
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         let sectionInfo = fetchedResultsController.sections![section]
         return sectionInfo.numberOfObjects
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell", for: indexPath) as! LocationCell
-
+        
         let location = fetchedResultsController.object(at: indexPath)
         cell.configure(for: location)
-    
+        
         return cell
     }
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let sectionInfo = fetchedResultsController.sections![section]
+        return sectionInfo.name
     }
-    */
-
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            let location = fetchedResultsController.object(at: indexPath)
+            managedObjectContext.delete(location)
+            do {
+                try managedObjectContext.save()
+            } catch{
+                fatalCoreDataError(error)
+            }
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
-
+    
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "EditLocation"{
@@ -126,5 +130,69 @@ class LocationsViewController: UITableViewController, NSFetchedResultsController
         } catch{
             fatalCoreDataError(error)
         }
+    }
+}
+
+// MARK:- NSFetchedResultsController Delegate Extension
+extension LocationsViewController {
+    func controllerWillChangeContent(_ controller:
+                                        NSFetchedResultsController<NSFetchRequestResult>) {
+        print("*** controllerWillChangeContent")
+        tableView.beginUpdates()
+    }
+    func controller(_ controller:
+                        NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange anObject: Any, at indexPath: IndexPath?,
+                    for type: NSFetchedResultsChangeType,
+                    newIndexPath: IndexPath?) {
+        switch type {
+            case .insert:
+                print("*** NSFetchedResultsChangeInsert (object)")
+                tableView.insertRows(at: [newIndexPath!], with: .fade)
+            case .delete:
+                print("*** NSFetchedResultsChangeDelete (object)")
+                tableView.deleteRows(at: [indexPath!], with: .fade)
+            case .update:
+                print("*** NSFetchedResultsChangeUpdate (object)")
+                if let cell = tableView.cellForRow(at: indexPath!)
+                    as? LocationCell {
+                    let location = controller.object(at: indexPath!)
+                        as! Location
+                    cell.configure(for: location)
+                }
+            case .move:
+                print("*** NSFetchedResultsChangeMove (object)")
+                tableView.deleteRows(at: [indexPath!], with: .fade)
+                tableView.insertRows(at: [newIndexPath!], with: .fade)
+            @unknown default:
+                fatalError("Unhandled switch case of NSFetchedResultsChangeType")
+        }
+    }
+    func controller(_ controller:
+                        NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange sectionInfo: NSFetchedResultsSectionInfo,
+                    atSectionIndex sectionIndex: Int,
+                    for type: NSFetchedResultsChangeType) {
+        switch type {
+            case .insert:
+                print("*** NSFetchedResultsChangeInsert (section)")
+                tableView.insertSections(IndexSet(integer: sectionIndex),
+                                         with: .fade)
+            case .delete:
+                print("*** NSFetchedResultsChangeDelete (section)")
+                tableView.deleteSections(IndexSet(integer: sectionIndex),
+                                         with: .fade)
+            case .update:
+                print("*** NSFetchedResultsChangeUpdate (section)")
+            case .move:
+                print("*** NSFetchedResultsChangeMove (section)")
+            @unknown default:
+                fatalError("Unhandled switch case of NSFetchedResultsChangeType")
+        }
+    }
+    func controllerDidChangeContent(_ controller:
+                                        NSFetchedResultsController<NSFetchRequestResult>) {
+        print("*** controllerDidChangeContent")
+        tableView.endUpdates()
     }
 }
