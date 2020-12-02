05987525e1929860313e4156f25c64f49e7bf682
TN <nxthao0701@gmail.com>
2020-12-02 22:52:01 +0700

    [IOS] Create the search app

--- Application/UIKit/StoreSearch/StoreSearch/StoreSearchViewController.swift
+++ Application/UIKit/StoreSearch/StoreSearch/StoreSearchViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class StoreSearchViewController: UIViewController {
 
+    // MARK: - Outlet variable
+    @IBOutlet weak var searchBar : UISearchBar!
+    @IBOutlet weak var tableView : UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     }
 }
 
+extension StoreSearchViewController : UISearchBarDelegate{
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("The search text is '\(searchBar.text!)'")
+    }
+}
