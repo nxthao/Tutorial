9775497cdf22d018883e6d7aaef44fe103b54e42
TN <nxthao0701@gmail.com>
2020-12-03 06:23:48 +0700

    [IOS] Create search bar and table view

--- Application/UIKit/StoreSearch/StoreSearch/StoreSearchViewController.swift
+++ Application/UIKit/StoreSearch/StoreSearch/StoreSearchViewController.swift
@@ -13,6 +13,9 @@ class StoreSearchViewController: UIViewController {
     @IBOutlet weak var searchBar : UISearchBar!
     @IBOutlet weak var tableView : UITableView!
     
+    // Variable
+    var searchResults = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
@@ -21,6 +24,28 @@ class StoreSearchViewController: UIViewController {
 
 extension StoreSearchViewController : UISearchBarDelegate{
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        print("The search text is '\(searchBar.text!)'")
+        searchResults = []
+        for i in 0...2{
+            searchResults.append(String(format: "Fake Result %d for '%@'", i, searchBar.text!))
+        }
+        tableView.reloadData()
+    }
+}
+
+extension StoreSearchViewController : UITableViewDelegate , UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "SearchResultCell"
+        
+        var cell : UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if cell == nil{
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+        }
+        
+        cell.textLabel!.text = searchResults[indexPath.row]
+        return cell
     }
 }
