95d31612699eb6465179ee474464f221097cfd9f
TN <nxthao0701@gmail.com>
2020-12-02 22:14:19 +0700

    [IOS] Create new app

---
+++ Application/UIKit/StoreSearch/StoreSearch/StoreSearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StoreSearchViewController.swift
+//  StoreSearch
+//
+//  Created by Thao Nguyen on 02/12/2020.
+//
+
+import UIKit
+
+class StoreSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
