0891b684bcfa778d918e57f6e69d8499a8b4e741
TN <nxthao0701@gmail.com>
2021-06-23 08:08:54 +0700

    [Flutter] Update bloc

--- Flutter/State_Management/bloc_state_management/lib/main.dart
+++ Flutter/State_Management/bloc_state_management/lib/main.dart
@@ -1,28 +1,70 @@
 import 'package:flutter/material.dart';
+import "package:flutter_bloc/flutter_bloc.dart";
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: ,
+    return BlocProvider(create: (context) => CounterBloc(), child: const MyApp1());
+  }
+}
+
+class MyApp1 extends StatelessWidget {
+  const MyApp1();
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: DemoPage(),
+    );
+  }
+}
+
+enum CounterEvent {
+  increment,
+  decrement
+}
+
+class CounterBloc extends Bloc<CounterEvent, int>{
+  CounterBloc() : super(0);
+
+  @override
+  Stream<int> mapEventToState(CounterEvent event) async* {
+    switch (event){
+      case CounterEvent.increment:
+        // state' is a getter defined inside Bloc<E,S> which // represents the current state of the bloc
+        yield state + 1;
+        break;
+      case CounterEvent.decrement:
+        yield state - 1;
+        break;
+    }
+  }
+}
+
+class DemoPage extends StatelessWidget{
+  const DemoPage();
+
+  @override
+  Widget build(BuildContext context) {
+    final counterBloc = BlocProvider.of<CounterBloc>(context);
+    return Scaffold(
+      body: Center(child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        TextButton(
+          onPressed: () => counterBloc.add(CounterEvent.increment),
+          child: Text("+1", style: TextStyle(color: Colors.green, fontSize: 25),)),
+        BlocBuilder<CounterBloc, int>(
+          builder: (context, count) => Text("$count", style: const TextStyle(fontSize: 30),),
+        ),
+        TextButton(onPressed: () => counterBloc.add(CounterEvent.decrement), child: Text("-1", style: TextStyle(color: Colors.red, fontSize: 25), )),
+
+          ],),),
     );
   }
 }
